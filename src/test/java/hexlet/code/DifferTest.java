1a1d8ebffa572a94bbfbb2bc4498fa2f9c0f28b3
Alexande Hamburg <uplink205@gmail.com>
2024-01-27 12:33:18 +0200

    update DifferTest.java

--- src/test/java/hexlet/code/DifferTest.java
+++ src/test/java/hexlet/code/DifferTest.java
@@ -5,15 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class DifferTest {
     @Test
     public void generateTest() throws Exception {
-        var expected = """
-                {
-                  - follow: false
-                    host: hexlet.io
-                  - proxy: 123.234.53.22
-                  - timeout: 50
-                  + timeout: 20
-                  + verbose: true
-                }""";
+        var expected = "{\n"
+                + "  - follow: false\n"
+                + "    host: hexlet.io\n"
+                + "  - proxy: 123.234.53.22\n"
+                + "  - timeout: 50\n"
+                + "  + timeout: 20\n"
+                + "  + verbose: true\n"
+                + "}";
         var actual = Differ.generate("file1.json", "file2.json");
         assertEquals(expected, actual);
     }
