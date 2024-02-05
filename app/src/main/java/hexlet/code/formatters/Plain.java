09c04ca1290df15be06aebe8849f3555545bce45
Alexande Hamburg <uplink205@gmail.com>
2024-02-05 14:30:46 +0200

    update Plain.java

--- app/src/main/java/hexlet/code/formatters/Plain.java
+++ app/src/main/java/hexlet/code/formatters/Plain.java
@@ -11,8 +11,8 @@ public class Plain {
         for (var mapOfDiffs : listOfMapDiffs) {
             var getKey = mapOfDiffs.get("key");
             var getStatus = mapOfDiffs.get("status");
-            var getValue1 = complexValueToString(mapOfDiffs.get("value1"));
-            var getValue2 = complexValueToString(mapOfDiffs.get("value2"));
+            var getValue1 = valueToPlainFormat(mapOfDiffs.get("value1"));
+            var getValue2 = valueToPlainFormat(mapOfDiffs.get("value2"));
 
             switch (String.valueOf(getStatus)) {
                 case "added":
@@ -32,13 +32,17 @@ public class Plain {
         return String.join("\n", result);
     }
 
-    public static Object complexValueToString(Object value) {
+    public static Object valueToPlainFormat(Object value) {
         var isInteger = value instanceof Integer;
         var isString = value instanceof String;
         var isBoolean = value instanceof Boolean;
         var isNull = value != null;
 
-        if (!isInteger && !isString && !isBoolean && isNull) {
+        if (isString) {
+            return "'" + value + "'";
+        }
+
+        if (!isInteger  && !isBoolean && isNull) {
             return "[complex value]";
         }
 
